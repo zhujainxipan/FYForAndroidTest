4e5e8f27fe99456ee6abcb95d2fad7f877e1e25f
安诺爱成长 <annuoaichengzhang@fiveyeardeMacBook-Pro.local>
2016-11-04 10:04:33 +0800

    导入相关资源

---
+++ app/src/main/java/com/ht/fyforandroid/widget/filter/FilterTestActivity.java
@@ -0,0 +1,31 @@
+package com.ht.fyforandroid.widget.filter;
+
+import android.os.Bundle;
+
+import com.ht.fyforandroid.R;
+import com.ht.fyforandroid.base.BaseActivity;
+
+/**
+ * Created by niehongtao on 16/11/4.
+ */
+public class FilterTestActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.filter_test;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
